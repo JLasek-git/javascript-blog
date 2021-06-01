78e761d70ae1b669fc47365015de3aaed657b5c6
Jakub Lasek <jakublasek47@gmail.com>
2021-06-02 01:22:16 +0200

    Add eslint, editorconfig and stylelint

--- js/script.js
+++ js/script.js
@@ -1,40 +1,40 @@
 'use strict';
 
 function titleClickHandler(event){
-    event.preventDefault();
-    const clickedElement = this;
-    console.log('Link was clicked!');
+  event.preventDefault();
+  const clickedElement = this;
+  console.log('Link was clicked!');
 
   /*[DONE] remove class 'active' from all article links  */
 
-    const activeLinks = document.querySelectorAll('.titles a.active');
-    for(let activeLink of activeLinks){
-        activeLink.classList.remove('active');
-    }
+  const activeLinks = document.querySelectorAll('.titles a.active');
+  for(let activeLink of activeLinks){
+    activeLink.classList.remove('active');
+  }
 
   /*[DONE] add class 'active' to the clicked link */
 
-    clickedElement.classList.add('active');
+  clickedElement.classList.add('active');
 
   /*[DONE] remove class 'active' from all articles */
 
-    const activeArticles = document.querySelectorAll('.posts article.active');
-    for(let activeArticle of activeArticles){
-        activeArticle.classList.remove('active');
-    }
+  const activeArticles = document.querySelectorAll('.posts article.active');
+  for(let activeArticle of activeArticles){
+    activeArticle.classList.remove('active');
+  }
 
   /*[DONE] get 'href' attribute from the clicked link */
 
-    const articleId = clickedElement.getAttribute('href');
-    console.log(articleId);
+  const articleId = clickedElement.getAttribute('href');
+  console.log(articleId);
 
   /*[DONE] find the correct article using the selector (value of 'href' attribute) */
 
-    const chosenArticle = document.querySelector(articleId);
+  const chosenArticle = document.querySelector(articleId);
 
   /*[DONE] add class 'active' to the correct article */
 
-    chosenArticle.classList.add('active');
+  chosenArticle.classList.add('active');
 }
 
 
@@ -43,7 +43,7 @@ const optArticleSelector = '.post',
   optTitleListSelector = '.titles';
 
 function generateTitleLinks(){
-console.log("Działa");
+  console.log('Działa');
   /*[DONE] remove contents of titleList */
   const titleList = document.querySelector(optTitleListSelector);
   titleList.innerHTML = '';
